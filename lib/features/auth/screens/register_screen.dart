76f3d2d6a260b802543d9b0303b6c728d30647f5
Cletus-Tech <ogijicletus22@gmail.com>
2026-08-02 02:08:45 +0100

    Fix missing result.dart imports and CardThemeData compat error

--- lib/features/auth/screens/register_screen.dart
+++ lib/features/auth/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../core/utils/result.dart';
 import '../../../core/routes/app_routes.dart';
 import '../../../core/utils/validators.dart';
 import '../../../services/firebase/auth_service.dart';
